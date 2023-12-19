48740d56d2056ba9e25842e6d574e7fef9b1f783
«sara21h» <sbogdan@iesebre.com>
2023-12-20 00:19:16 +0100

    a

--- script.js
+++ script.js
@@ -1,35 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
-    particlesJS("particles-js", {
-      particles: {
-        number: { value: 50, density: { enable: true, value_area: 800 } },
-        color: { value: "#689f38" }, // Color de la hierba
-        shape: {
-          type: "edge", // Cambiado a "edge" para que parezca hierba
-          options: {
-            edge: {
-              width: 2,
-              height: 5
-            },
-            polygon: {
-              nb_sides: 6
-            },
-            image: {
-              src: "img/github.svg",
-              width: 100,
-              height: 100
-            }
-          }
-        },
-        opacity: { value: 0.5, random: true, anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false } },
-        size: { value: 3, random: true, anim: { enable: false, speed: 40, size_min: 0.1, sync: false } },
-        line_linked: { enable: false },
-        move: { enable: true, speed: 0.5, direction: "bottom", random: true, straight: false, out_mode: "out", bounce: false, attract: { enable: false, rotateX: 600, rotateY: 1200 } }
-      },
-      interactivity: {
-        detect_on: "canvas",
-        events: { onhover: { enable: false, mode: "repulse" }, onclick: { enable: false, mode: "push" }, resize: true },
-      },
-      retina_detect: true
-    });
+  particlesJS("particles-js", {
+    particles: {
+      number: { value: 50, density: { enable: true, value_area: 800 } },
+      color: { value: "#689f38" }, // Color de la hierba
+      shape: { type: "circle" },
+      opacity: { value: 0.5, random: true, anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false } },
+      size: { value: 3, random: true, anim: { enable: false, speed: 40, size_min: 0.1, sync: false } },
+      line_linked: { enable: false },
+      move: { enable: true, speed: 0.5, direction: "bottom", random: true, straight: false, out_mode: "out", bounce: false, attract: { enable: false, rotateX: 600, rotateY: 1200 } }
+    },
+    interactivity: {
+      detect_on: "canvas",
+      events: { onhover: { enable: false, mode: "repulse" }, onclick: { enable: false, mode: "push" }, resize: true },
+    },
+    retina_detect: true
   });
-  
+});
