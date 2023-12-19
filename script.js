66be67b72ac1ac953c80b9e633a882f69d1e7b8a
«sara21h» <sbogdan@iesebre.com>
2023-12-19 23:52:37 +0100

    a

--- script.js
+++ script.js
@@ -3,7 +3,23 @@ document.addEventListener("DOMContentLoaded", function() {
       particles: {
         number: { value: 50, density: { enable: true, value_area: 800 } },
         color: { value: "#689f38" }, // Color de la hierba
-        shape: { type: "circle" },
+        shape: {
+          type: "edge", // Cambiado a "edge" para que parezca hierba
+          options: {
+            edge: {
+              width: 2,
+              height: 5
+            },
+            polygon: {
+              nb_sides: 6
+            },
+            image: {
+              src: "img/github.svg",
+              width: 100,
+              height: 100
+            }
+          }
+        },
         opacity: { value: 0.5, random: true, anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false } },
         size: { value: 3, random: true, anim: { enable: false, speed: 40, size_min: 0.1, sync: false } },
         line_linked: { enable: false },
