1fa4d029064d5dda63f72351c78f2a8f3eedef98
sasa0sa <151135533+sasa0sa@users.noreply.github.com>
2023-12-22 12:23:41 +0100

    a

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ document.addEventListener("DOMContentLoaded", function() {
   particlesJS("particles-js", {
     particles: {
       number: { value: 50, density: { enable: true, value_area: 800 } },
-      color: { value: "#689f38" }, // Color de la hierba
+      color: { value: "#0ea5e9" }, // Color de la hierba
       shape: { type: "circle" },
       opacity: { value: 0.5, random: true, anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false } },
-      size: { value: 3, random: true, anim: { enable: false, speed: 40, size_min: 0.1, sync: false } },
+      size: { value: 5, random: true, anim: { enable: false, speed: 40, size_min: 0.1, sync: false } },
       line_linked: { enable: false },
       move: { enable: true, speed: 0.5, direction: "bottom", random: true, straight: false, out_mode: "out", bounce: false, attract: { enable: false, rotateX: 600, rotateY: 1200 } }
     },
